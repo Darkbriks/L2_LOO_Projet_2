a07f1136f8f7d3e3bbd8939427a940bfeb8810d4
Darkbriks <antoine.patron.62610@gmail.com>
2024-05-09 12:04:36 +0200

    Init repos

---
+++ desktop/src/com/scramble_like/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.scramble_like.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.scramble_like.game.ScrambleLikeApplication;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("ScrambleLike");
+		new Lwjgl3Application(new ScrambleLikeApplication(), config);
+	}
+}
