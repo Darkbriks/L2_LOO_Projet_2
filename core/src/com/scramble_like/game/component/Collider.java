a9a5b07277e59d93c2ba8ba38a2b89cee15569d1
Zyre28 <babies11@hotmail.fr>
2024-05-14 19:58:11 +0200

    Update de Collider (detect) + test (//TODO)

--- core/src/com/scramble_like/game/component/Collider.java
+++ core/src/com/scramble_like/game/component/Collider.java
@@ -1,52 +1,130 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package com.scramble_like.game.component;
 
 import com.badlogic.gdx.math.Rectangle;
 import com.scramble_like.game.essential.Component;
 import com.scramble_like.game.essential.GameObject;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Collider extends Component {
     private Rectangle hitbox;
-    public Collider() {
-        super();
-        hitbox = new Rectangle();
+    private boolean generateOverlappedEvent = false;
+    private float width;
+    private float height;
+    private float x;
+    private float y;
+    private boolean simulatePhysics;
+
+    public Collider(float x, float y, float w, float h) {
+        this.x = this.getOwner().getTransform().getLocation().x;
+        this.y = this.getOwner().getTransform().getLocation().y;
+        this.simulatePhysics = false;
+        this.hitbox = new Rectangle();
+        this.hitbox.x = x;
+        this.hitbox.y = y;
+        this.hitbox.height = h;
+        this.hitbox.width = h;
+    }
+
+    public void setHeight(int height) {
+        this.hitbox.height = (float)height;
+        this.height = (float)height;
+    }
+
+    public float getHeight() {
+        return this.height;
+    }
+
+    public void setWidth(int width) {
+        this.hitbox.width = (float)width;
+        this.width = (float)width;
+    }
+
+    public float getWidth() {
+        return this.width;
+    }
+
+    public float getX() {
+        return this.x;
+    }
+
+    public float getY() {
+        return this.y;
+    }
+
+    public void setGenerateOverlapedEvent(boolean generateOverlappedEvent) {
+        this.generateOverlappedEvent = generateOverlappedEvent;
+    }
+
+    public boolean isGenerateOverlappedEvent() {
+        return this.generateOverlappedEvent;
     }
 
     public Rectangle getHitbox() {
-        return hitbox;
+        return this.hitbox;
     }
 
-    public void setHitbox(Rectangle hitbox) {
-        this.hitbox = hitbox;
+    public void setHitbox(int w, int h) {
+        this.setWidth(w);
+        this.setHeight(h);
     }
-    //Renvoi Owner si une collision a eu lieu entre 2 Owners (celui de l'appel et celui en paramètre)
-    private GameObject hit(Collider collision){
-        if(getOwner().getTransform().getLocation().x==collision.getOwner().getTransform().getLocation().x){
-            if(getOwner().getTransform().getLocation().y==collision.getOwner().getTransform().getLocation().y){
-                if(getOwner().getTransform().getLocation().z==collision.getOwner().getTransform().getLocation().z){
-                    return getOwner();
+
+    private void detect(GameObject other) {
+        ArrayList<Collider> l = other.GetAllComponentsFromClass(Collider.class);
+        Iterator var3 = l.iterator();
+
+        while(true) {
+            while(var3.hasNext()) {
+                Collider c = (Collider)var3.next();
+                float left1 = c.hitbox.x;
+                float right1 = c.hitbox.x + c.hitbox.width;
+                float top1 = c.hitbox.y;
+                float bottom1 = c.hitbox.y + c.hitbox.height;
+                float left2 = this.hitbox.x;
+                float right2 = this.hitbox.x + this.hitbox.width;
+                float top2 = this.hitbox.y;
+                float bottom2 = this.hitbox.y + this.hitbox.height;
+                if (right1 >= left2 && left1 <= right2 && bottom1 >= top2 && top1 <= bottom2 && this.isGenerateOverlappedEvent()) {
+                    this.beginOverllap(c);
+                } else {
+                    this.hit(c);
                 }
-                else{return null;}
             }
-            else{return null;}
+
+            return;
         }
-        else{return null;}
     }
 
-    @Override
-    public void BeginPlay() {
-
+    private GameObject hit(Collider collision) {
+        return null;
     }
 
-    @Override
+    private GameObject beginOverllap(Collider collision) {
+        return null;
+    }
+
+    private GameObject endOverllap(Collider collision) {
+        return null;
+    }
+
     public void Update(double DeltaTime) {
+        ArrayList<GameObject> g = new ArrayList();
+        g.add(this.Owner);
+        ArrayList<GameObject> l = this.getOwner().getScene().GetGameObjects(g);
+        Iterator var5 = l.iterator();
+
+        while(var5.hasNext()) {
+            GameObject iter = (GameObject)var5.next();
+            this.detect(iter);
+        }
 
     }
 
-    @Override
     public void Render() {
-
     }
-
-
-
 }
