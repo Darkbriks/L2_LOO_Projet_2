ffe849b4571e20d4d5c9da91e3af92a4fd8148fd
Zyre28 <babies11@hotmail.fr>
2024-05-15 14:59:53 +0200

    Updated(/Added) GameConstant,DesktopLauncher. Addded Background, Chunk. (Opérationnel)

--- core/src/com/scramble_like/game/GameConstant.java
+++ core/src/com/scramble_like/game/GameConstant.java
@@ -4,4 +4,7 @@ public class GameConstant
 {
     public static int WIDTH = 1600;
     public static int HEIGHT = 900;
+    public static int SQUARE_SIDE = 10;
+
+    public static int CHUNK_SIZE = 500;
 }
