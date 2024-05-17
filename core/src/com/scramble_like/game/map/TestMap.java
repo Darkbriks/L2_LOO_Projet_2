1dbc953e3e9b6aa9c384b082a0a1f0643073f5a7
Darkbriks <antoine.patron.62610@gmail.com>
2024-05-17 15:21:01 +0200

    add test map

--- core/src/com/scramble_like/game/map/TestMap.java
+++ core/src/com/scramble_like/game/map/TestMap.java
@@ -1,23 +1,16 @@
 package com.scramble_like.game.map;
 
-import com.badlogic.gdx.Game;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.scramble_like.game.ScrambleLikeApplication;
-import com.scramble_like.game.component.TestComponent;
+import com.scramble_like.game.component.Tile;
 import com.scramble_like.game.essential.GameObject;
 import com.scramble_like.game.essential.Scene;
-import com.scramble_like.game.essential.event_dispatcher.EventIndex;
-import com.scramble_like.game.essential.event_dispatcher.EventListener;
-import com.scramble_like.game.essential.event_dispatcher.event.chunk.ChunkLoadedEvent;
 import com.scramble_like.game.essential.exception.GameIsNullException;
 import com.scramble_like.game.essential.exception.SceneIsNullException;
-import com.scramble_like.game.essential.event_dispatcher.EventDispatcher;
 import com.scramble_like.game.game_object.ChunkManager;
-import com.scramble_like.game.game_object.Enemy;
 import com.scramble_like.game.game_object.Player;
-import com.scramble_like.game.utils.Chunk;
-
-import java.util.EventObject;
+import com.scramble_like.game.game_object.enemy.MovingEnemy;
 
 public class TestMap extends Scene
 {
@@ -27,7 +20,7 @@ public class TestMap extends Scene
 
         try
         {
-            ChunkManager chunkManager = new ChunkManager("ChunkManager", this, 0, 0);
+            ChunkManager chunkManager = new ChunkManager("ChunkManager", this, 0, -500);
             AddGameObject(chunkManager);
 
             Player go1 = new Player("Player", this, new Vector3(-350, 0, 0));
@@ -35,8 +28,13 @@ public class TestMap extends Scene
 
             chunkManager.setPlayer(go1);
 
+            /*GameObject go2 = new GameObject("GameObject", this);
+            go2.AddComponent(new Tile("badlogic.jpg", 0, 0));
+            AddGameObject(go2);*/
+
             //AddGameObject(new Projectile("Projectile", this, go1, new Vector3(500, 0, 0)));
-            //AddGameObject(new Enemy("Enemy", this, 100, 100, true, go1, "badlogic.jpg", 10));
+            Vector2[] waypoints = {new Vector2(0, 300), new Vector2(300, 300), new Vector2(300, 0), new Vector2(0, 0)};
+            AddGameObject(new MovingEnemy("Enemy", this, "badlogic.jpg", 100, false, null, waypoints, 150));
         }
         catch (SceneIsNullException e) { System.out.println("Error: " + e.getMessage()); }
 
