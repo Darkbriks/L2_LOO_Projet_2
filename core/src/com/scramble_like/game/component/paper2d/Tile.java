5f4ed812a50745accc92982adfc12a7c32b2246f
Darkbriks <antoine.patron.62610@gmail.com>
2024-05-18 15:54:07 +0200

    Add a size for Tile

--- core/src/com/scramble_like/game/component/paper2d/Tile.java
+++ core/src/com/scramble_like/game/component/paper2d/Tile.java
@@ -6,13 +6,24 @@ import com.scramble_like.game.essential.factory.ImageFactory;
 
 public class Tile extends Sprite
 {
-    private float x, y;
+    private float x, y, width, height;
 
     public Tile(String path, float x, float y)
     {
         super(path);
         this.x = x;
         this.y = y;
+        this.width = GameConstant.SQUARE_SIDE;
+        this.height = GameConstant.SQUARE_SIDE;
+    }
+
+    public Tile(String path, float x, float y, float width, float height)
+    {
+        super(path);
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
     }
 
     public void addOffset(Vector2 offset) { x -= offset.x; y -= offset.y; }
@@ -21,6 +32,6 @@ public class Tile extends Sprite
     public void Render()
     {
         if (!this.IsActive()) { return; }
-        this.getOwner().getBatch().draw(ImageFactory.getTexture(this.fileName), this.x, this.y, GameConstant.SQUARE_SIDE, GameConstant.SQUARE_SIDE);
+        this.getOwner().getBatch().draw(ImageFactory.getTexture(this.fileName), this.x, this.y, width, height);
     }
 }
