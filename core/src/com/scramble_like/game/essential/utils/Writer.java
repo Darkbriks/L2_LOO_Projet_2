66c3845553eb760bfeb7f68f185b9ca2d9b7acc0
Zyre28 <babies11@hotmail.fr>
2024-05-22 21:09:47 +0200

    Added things

--- core/src/com/scramble_like/game/essential/utils/Writer.java
+++ core/src/com/scramble_like/game/essential/utils/Writer.java
@@ -10,52 +10,102 @@ import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
-public class Writer {
 
-    public Writer(String valeur, String option, String fileName) {
-        File file = new File("D:\\libGDX\\L2_LOO_Projet_2\\", fileName);
+public class Writer {
+    public Writer(String valeur, String option, String fileName, Map<Integer, String> list) {
+        String currentDir = System.getProperty("user.dir");
+        File file = new File(currentDir, fileName);
         System.out.println(file.getAbsolutePath());
 
+        // Crée le fichier s'il n'existe pas
         if (!file.exists()) {
             try {
                 if (file.createNewFile()) {
-                    System.out.println("File created successfully!");
+                    //System.out.println("File created successfully!");
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        } else {
-            System.out.println("File already exists! Overwriting content.");
-        }
+        } /*else {
+            //System.out.println("File already exists! Updating content.");
+        }*/
 
-        boolean isFind = false;
-        try (PrintWriter writer = new PrintWriter(file)) {
-            try {
-                List<String> lines = Files.readAllLines(Paths.get(file.getAbsolutePath()));
-                for (String line : lines) {
-                    for (int i = 0; i < GameConstant.HIGHSCORE_LIST.size(); i++) {
-                        String firstWord = line.split("\\s+")[0];
-                        System.out.println(firstWord);
-                        if (Objects.equals(option, firstWord)) {
-                            writer.println(option + " : " + valeur);
-                            isFind = true;
-                            System.out.println("File rewritten successfully!");
-                        } else {
-                            writer.println(line);
-                        }
-                    }
-                }
-                if (!isFind) {
-                    writer.println(option + " : " + valeur);
-                    System.out.println("File written successfully!");
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+        int oldHighscore= Writer.getSetting(valeur, option, fileName, list);
+        if(oldHighscore!=-1){
+            if(oldHighscore>Integer.parseInt(valeur)){
+                return;
             }
+        }
+        boolean isFind = false;
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(file.getAbsolutePath()));
+            for (String line : lines) {
+                String firstWord = line.split("\\s+")[0];
+                //System.out.println("Line : "+line);
+                //System.out.println("FirstWord : "+firstWord);
+                if (Objects.equals(option, firstWord)) {
+                    contentBuilder.append(option).append(" : ").append(valeur).append("\n");
+                    isFind = true;
+                    //System.out.println("File updated successfully!");
+                } else {
+                    contentBuilder.append(line).append("\n");
+                }
+            }
+
+            // Si l'option n'a pas été trouvée, l'ajouter à la fin
+            if (!isFind) {
+                contentBuilder.append(option).append(" : ").append(valeur).append("\n");
+                //System.out.println("New entry added successfully!");
+            }
+
+            // Écrire tout le contenu mis à jour dans le fichier
+            try (FileWriter writer = new FileWriter(file)) {
+                writer.write(contentBuilder.toString());
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static int getSetting(String valeur, String option, String fileName, Map<Integer, String> list) {
+        String currentDir = System.getProperty("user.dir");
+        File file = new File(currentDir, fileName);
+        //System.out.println(file.getAbsolutePath());
+
+        // Crée le fichier s'il n'existe pas
+        if (!file.exists()) {
+            return -1;
+        } else {
+            try {
+                List<String> lines = Files.readAllLines(Paths.get(file.getAbsolutePath()));
+                for (String line : lines) {
+                    String firstWord = line.split("\\s+")[0];
+                    //System.out.println("Line: " + line);
+                    //System.out.println("FirstWord: " + firstWord);
+                    if (Objects.equals(option, firstWord)) {
+                        //System.out.println("Values of " + option + " found successfully: " + findLastWord(line));
+                        return Integer.parseInt(findLastWord(line));
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return -1;
+    }
+
+    public static String findLastWord(String line) {
+        if (line == null || line.trim().isEmpty()) {
+            return null; // Return null if the line is null or empty
+        }
+
+        String[] words = line.trim().split("\\s+"); // Split the line into words using whitespace as the delimiter
+        return words[words.length - 1]; // Return the last word
     }
 }
--- core/src/com/scramble_like/game/map/AbstractLevel.java
+++ core/src/com/scramble_like/game/map/AbstractLevel.java
@@ -6,8 +6,11 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.scramble_like.game.GameConstant;
 import com.scramble_like.game.component.controller.PlayerController;
 import com.scramble_like.game.essential.Scene;
+import com.scramble_like.game.essential.chunk.ChunkHelper;
 import com.scramble_like.game.essential.chunk.ChunkManager;
 import com.scramble_like.game.essential.exception.SceneIsNullException;
+import com.scramble_like.game.essential.factory.ImageFactory;
+import com.scramble_like.game.essential.factory.SoundFactory;
 import com.scramble_like.game.essential.utils.Writer;
 import com.scramble_like.game.game_object.Background;
 import com.scramble_like.game.game_object.LevelLoader;
@@ -19,16 +22,27 @@ import static java.lang.Math.round;
 
 public abstract class AbstractLevel extends Scene
 {
-    private Label scoreActor;
+    private Label scoreActor,highscoreActor;
     private LifeActor lifeActor;
 
     private int currentframe;
 
+    private int level;
+
     public AbstractLevel(String name, int level, Vector2 levelLoaderLocation)
     {
         super(name);
+        this.level=level;
+        SoundFactory.getInstance().loadSound("damage_taken.mp3","Audio/Sound/damage_taken.mp3");
+        SoundFactory.getInstance().loadSound("dead","Audio/Sound/dead.mp3");
 
         getCamera().setPosition(0, 0);
+
+        for (int i = 0; i < ChunkHelper.getTileMapSize(); i++)
+        {
+            String path = ChunkHelper.getTilePath(i);
+            if (path != null && !path.isEmpty()) { ImageFactory.loadTexture(ChunkHelper.getTilePath(i)); }
+        }
 
         try
         {
@@ -43,8 +57,9 @@ public abstract class AbstractLevel extends Scene
             chunkManager.setPlayer(go1);
 
             LevelLoader levelLoader = new LevelLoader("LevelLoader", this, (level + 1) % GameConstant.LEVEL_LIST.size());
-            levelLoader.getTransform().setLocation(levelLoaderLocation);
+            levelLoader.getTransform().setLocation(300,0);
             AddGameObject(levelLoader);
+
         }
         catch (SceneIsNullException e) { Gdx.app.error("Abstract", "Error: " + e.getMessage()); }
 
@@ -63,6 +78,11 @@ public abstract class AbstractLevel extends Scene
         scoreActor.setPosition(-700,390);
         scoreActor.setFontScale(1.5f,1.5f);
         this.getStage().addActor(scoreActor);
+
+        highscoreActor = new AE_Label( "HighScore : "+ Writer.getSetting(String.valueOf(getPlayer().getPlayerController().getScore()), "highscore_"+level,"highscore.txt",GameConstant.HIGHSCORE_LIST), this.getSkin());
+        highscoreActor.setPosition(0,0);
+        highscoreActor.setFontScale(1.5f,1.5f);
+        this.getStage().addActor(highscoreActor);
     }
 
     private void newLife()
@@ -77,6 +97,7 @@ public abstract class AbstractLevel extends Scene
     {
         this.scoreActor.setText("Score : " + this.getPlayer().GetFirstComponentFromClass(PlayerController.class).getScore() + "  FPS : " + Gdx.graphics.getFramesPerSecond());
         this.scoreActor.setPosition(getCamera().getPosition().x - GameConstant.SCORE_X, getCamera().getPosition().y + GameConstant.SCORE_Y);
+        this.highscoreActor.setPosition(getCamera().getPosition().x - GameConstant.SCORE_X, getCamera().getPosition().y + GameConstant.SCORE_Y);
         this.lifeActor.setPosition(getCamera().getPosition().x - GameConstant.LIFE_X, getCamera().getPosition().y + GameConstant.LIFE_Y);
         newLife();
 
@@ -89,6 +110,15 @@ public abstract class AbstractLevel extends Scene
     public void dispose()
     {
         super.dispose();
-        new Writer(String.valueOf(getPlayer().getPlayerController().getScore()), "highscore_1","highscore.txt");
+        new Writer(String.valueOf(getPlayer().getPlayerController().getScore()), "highscore_"+level,"highscore.txt",GameConstant.HIGHSCORE_LIST);
+        System.out.println(Writer.getSetting(String.valueOf(getPlayer().getPlayerController().getScore()), "highscore_"+level,"highscore.txt",GameConstant.HIGHSCORE_LIST));
+
+        SoundFactory.getInstance().unloadAllSounds();
+
+        for (int i = 0; i < ChunkHelper.getTileMapSize(); i++)
+        {
+            String path = ChunkHelper.getTilePath(i);
+            if (path != null && !path.isEmpty()) { ImageFactory.disposeTexture(ChunkHelper.getTilePath(i)); }
+        }
     }
 }
