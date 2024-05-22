0a27fbb5b57eb84965d243ffc44164023bb86d37
Zyre28 <babies11@hotmail.fr>
2024-05-22 23:04:24 +0200

    musics + writer + fix

--- core/src/com/scramble_like/game/map/AbstractLevel.java
+++ core/src/com/scramble_like/game/map/AbstractLevel.java
@@ -3,6 +3,7 @@ package com.scramble_like.game.map;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Widget;
 import com.scramble_like.game.GameConstant;
 import com.scramble_like.game.component.controller.PlayerController;
 import com.scramble_like.game.essential.Scene;
@@ -57,7 +58,7 @@ public abstract class AbstractLevel extends Scene
             chunkManager.setPlayer(go1);
 
             LevelLoader levelLoader = new LevelLoader("LevelLoader", this, (level + 1) % GameConstant.LEVEL_LIST.size());
-            levelLoader.getTransform().setLocation(300,0);
+            levelLoader.getTransform().setLocation(levelLoaderLocation.x,levelLoaderLocation.y);
             AddGameObject(levelLoader);
 
         }
@@ -71,7 +72,6 @@ public abstract class AbstractLevel extends Scene
         this.currentframe=0;
         lifeActor = new LifeActor(this.getStage(), "UI/heart.png", 5);
         lifeActor.setCurrentRegion(currentframe);
-        lifeActor.setPosition(730,400);
         this.getStage().addActor(lifeActor);
 
         scoreActor = new AE_Label( "Score : "+ this.getPlayer().GetFirstComponentFromClass(PlayerController.class).getScore(), this.getSkin());
@@ -79,7 +79,7 @@ public abstract class AbstractLevel extends Scene
         scoreActor.setFontScale(1.5f,1.5f);
         this.getStage().addActor(scoreActor);
 
-        highscoreActor = new AE_Label( "HighScore : "+ Writer.getSetting(String.valueOf(getPlayer().getPlayerController().getScore()), "highscore_"+level,"highscore.txt",GameConstant.HIGHSCORE_LIST), this.getSkin());
+        highscoreActor = new AE_Label( "HighScore : "+ Writer.getSetting( "highscore_"+level,"highscore.txt"), this.getSkin());
         highscoreActor.setPosition(0,0);
         highscoreActor.setFontScale(1.5f,1.5f);
         this.getStage().addActor(highscoreActor);
@@ -97,7 +97,7 @@ public abstract class AbstractLevel extends Scene
     {
         this.scoreActor.setText("Score : " + this.getPlayer().GetFirstComponentFromClass(PlayerController.class).getScore() + "  FPS : " + Gdx.graphics.getFramesPerSecond());
         this.scoreActor.setPosition(getCamera().getPosition().x - GameConstant.SCORE_X, getCamera().getPosition().y + GameConstant.SCORE_Y);
-        this.highscoreActor.setPosition(getCamera().getPosition().x - GameConstant.SCORE_X, getCamera().getPosition().y + GameConstant.SCORE_Y);
+        this.highscoreActor.setPosition(getCamera().getPosition().x - GameConstant.HIGHSCORE_X, getCamera().getPosition().y + GameConstant.HIGHSCORE_Y);
         this.lifeActor.setPosition(getCamera().getPosition().x - GameConstant.LIFE_X, getCamera().getPosition().y + GameConstant.LIFE_Y);
         newLife();
 
@@ -110,8 +110,8 @@ public abstract class AbstractLevel extends Scene
     public void dispose()
     {
         super.dispose();
-        new Writer(String.valueOf(getPlayer().getPlayerController().getScore()), "highscore_"+level,"highscore.txt",GameConstant.HIGHSCORE_LIST);
-        System.out.println(Writer.getSetting(String.valueOf(getPlayer().getPlayerController().getScore()), "highscore_"+level,"highscore.txt",GameConstant.HIGHSCORE_LIST));
+        Writer.writeSetting(String.valueOf(getPlayer().getPlayerController().getScore()), "highscore_"+level,"highscore.txt",GameConstant.HIGHSCORE_LIST);
+        System.out.println(Writer.getSetting( "highscore_"+level,"highscore.txt"));
 
         SoundFactory.getInstance().unloadAllSounds();
 
