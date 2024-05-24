d920a74c43fe4740596afbacb76aca2968e9a3d5
Darkbriks <antoine.patron.62610@gmail.com>
2024-05-24 08:12:28 +0200

    Add a text when a GoldenStrawberry is collected

--- core/src/com/scramble_like/game/game_object/effect/GoldenStrawberry.java
+++ core/src/com/scramble_like/game/game_object/effect/GoldenStrawberry.java
@@ -5,6 +5,7 @@ import com.scramble_like.game.essential.Scene;
 import com.scramble_like.game.essential.chaos.SphereCollider;
 import com.scramble_like.game.essential.exception.SceneIsNullException;
 import com.scramble_like.game.game_object.Player;
+import com.scramble_like.game.ui.AE_Label;
 
 public class GoldenStrawberry extends PowerUp
 {
@@ -20,6 +21,7 @@ public class GoldenStrawberry extends PowerUp
     protected void Triggered(Player player)
     {
         player.getPlayerController().setGoldenStrawberry(true);
+        getScene().getStage().addActor(new AE_Label("Golden Strawberry Collected!", getScene().getSkin(), 2, getScene().getEventDispatcher(), true));
         DestroyThisInScene();
     }
 }
